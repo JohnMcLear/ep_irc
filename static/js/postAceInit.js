65f554bf66b61da6bbd200c7cf3225240a96a5a8
John McLear <john@mclear.co.uk>
2013-03-24 18:29:47 +0000

    on by default

--- static/js/postAceInit.js
+++ static/js/postAceInit.js
@@ -1,12 +1,13 @@
 exports.postAceInit = function(hook, context){
 
-  chat.stickToScreen();
-  var authorName = pad.getUserName();
+  if($('#options-ircchat').is(':checked')) {
+    chat.stickToScreen();
+    var authorName = pad.getUserName();
+    var url= '<iframe src="http://webchat.freenode.net?nick='+authorName+'&channels=etherpad,etherpad-lite-dev&uio=MTE9MjA16a" width="400" height=100% frameBorder=0></iframe>';
 
-  var url= '<iframe src="http://webchat.freenode.net?nick='+authorName+'&channels=etherpad,etherpad-lite-dev&uio=MTE9MjA16a" width="400" height=100% frameBorder=0></iframe>';
-
-  $('#chatbox').html(url);
-  $('#editorcontainer').css({"right":"406px", "width":"auto"});
-  $('#chatbox').css("cssText", "width:405px !important;display:block;right:0px;top:37px;bottom:0px;padding:0;margin:0;height:auto;border:0;");
+    $('#chatbox').html(url);
+    $('#editorcontainer').css({"right":"406px", "width":"auto"});
+    $('#chatbox').css("cssText", "width:405px !important;display:block;right:0px;top:37px;bottom:0px;padding:0;margin:0;height:auto;border:0;");
+  }
 }
 
