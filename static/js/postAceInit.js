233b78acec9242f4cf07bf6ee88f0ec435a58462
John McLear <john@mclear.co.uk>
2013-03-24 18:33:44 +0000

    mheh

--- static/js/postAceInit.js
+++ static/js/postAceInit.js
@@ -1,6 +1,18 @@
 exports.postAceInit = function(hook, context){
 
   if($('#options-ircchat').is(':checked')) {
+    chatEnable();
+  }
+
+  $('#options-ircchat').on('click', function() {
+    if($('#options-ircchat').is(':checked')) {
+      chatEnable();
+    }else{
+      chatDisable();
+    }
+  });
+
+  function chatEnable(){
     chat.stickToScreen();
     var authorName = pad.getUserName();
     var url= '<iframe src="http://webchat.freenode.net?nick='+authorName+'&channels=etherpad,etherpad-lite-dev&uio=MTE9MjA16a" width="400" height=100% frameBorder=0></iframe>';
@@ -8,6 +20,10 @@ exports.postAceInit = function(hook, context){
     $('#chatbox').html(url);
     $('#editorcontainer').css({"right":"406px", "width":"auto"});
     $('#chatbox').css("cssText", "width:405px !important;display:block;right:0px;top:37px;bottom:0px;padding:0;margin:0;height:auto;border:0;");
+  }
+
+  function chatDisable(){
+    location.reload();
   }
 }
 
